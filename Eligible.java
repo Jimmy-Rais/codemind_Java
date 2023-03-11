eca943041c95fca05c9c52ed0db209088ec311db
Jimmy-Rais <raisgachaba@gmail.com>
2023-03-11 10:46:12 +0530

    Create Eligible.java

---
+++ Eligible.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Eligible 
+{
+public static void main(String args[])
+{
+ int age;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter your age:");
+age=sc.nextInt();
+ if (age>=18)
+{
+  System.out.println("You are eligible");
+}
+ else
+{
+ System.out.println("You are not eligible,wait for "+(18-age)+"years");
+}
+}
+}
