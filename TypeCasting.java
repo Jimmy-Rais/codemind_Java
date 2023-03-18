b8b7e24c3f3967dc2e5aff1f4f543ba7878cfca8
Jimmy-Rais <raisgachaba@gmail.com>
2023-03-18 10:45:24 +0530

    Create TypeCasting.java

---
+++ TypeCasting.java
@@ -0,0 +1,13 @@
+class TypeCasting {
+public static void main(String args[])
+{
+  float a=3.123f;
+  int b;
+  int c=2;
+  float d;
+ d=c;
+System.out.println("d="+d);
+ b=(int)a;
+System.out.println("b="+b);
+}
+}
