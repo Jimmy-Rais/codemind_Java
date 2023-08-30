0da72a124f0580f634d87c515c22ab1211e2f0ef
Jimmy-Rais <raisgachaba@gmail.com>
2023-08-30 18:14:46 +0530

    ,

--- FlowLayoutDem.java
+++ FlowLayoutDem.java
@@ -4,14 +4,21 @@ class FlowLayoutDem extends Frame{
 Label l1;
 TextField tf1;
 Button b1;
+CheckboxGroup cbg=new CheckboxGroup();
+Checkbox ch1,ch2;
 public FlowLayoutDem(){
 setLayout(new FlowLayout());
 l1=new Label("Username");
 tf1=new TextField(25);
 b1=new Button("Login");
+ch1=new Checkbox("Java",cbg,true);
+ch2=new Checkbox("C",cbg,false);
 add(l1);
 add(tf1);
 add(b1);
+add(ch1);
+add(ch2);
+
 }
 public static void main(String args[]){
 
